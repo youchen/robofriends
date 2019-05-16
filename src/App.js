b31629e7a62dd5b3ffdf9f904297104de6a6dadf
Youchen Ren <youchen.r@gmail.com>
2019-05-15 23:48:45 -0400

    Fetch user data from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import SearchField from './SearchField';
 import CardList from './CardList';
-import { robots } from './robots';
 import 'tachyons';
 import './App.css'
 
@@ -10,7 +9,7 @@ class App extends Component {
         super();
         this.state = {
             SearchField : '',
-            robotsOnScreen : robots
+            robotsOnScreen : []
         }
     }
 
@@ -32,6 +31,17 @@ class App extends Component {
                 <CardList users={filteredRobots} />
             </div>
         );
+    }
+
+    componentDidMount () {
+        /* 
+            WARN: response blcoked by CORS policy, used 
+            https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf?hl=en
+            as workaround
+        */
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robotsOnScreen: users}))
     }
 }
 
