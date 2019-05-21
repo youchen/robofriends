f3abb240990a7637dfcb347cc4b6dd370283a730
Youchen Ren <youchen.r@gmail.com>
2019-05-20 23:30:17 -0400

    Error handling

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({users}) => {
+    // Testing the ErrorBoundry
+    // if (true) {
+    //     throw error("Noooo!");
+    // }
     const CardArray = users.map((u, i) => {
         return (
             <Card key={i} id={users[i].id} name={users[i].name} email={users[i].email} />
